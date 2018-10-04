4571a5a91b70875895d945fa57216added84198d
Edward Whelan <vroxing@gmail.com>
2018-10-03 20:50:05 -0400

    troubleshooting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Link from 'gatsby-link'
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
